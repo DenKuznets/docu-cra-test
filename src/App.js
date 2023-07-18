161012f6e7f1cbb0f0a8a74216276a4508077709
Denis Kuznetsov <denis.kyznecov@gmail.com>
2023-07-18 08:53:44 +0300

    working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
@@ -6,6 +5,7 @@ import { styled } from "styled-components";
 import { Button } from "@mui/material";
 import { library, icon as someIcon } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Container = styled.div`
     display: flex;
@@ -20,15 +20,30 @@ const Content = styled.div`
     display: flex;
     flex-direction: column;
     gap: 30px;
-    .fa-user-secret {
+    svg {
         color: white;
     }
 `;
 
+library.add(fas);
+
 function App() {
+    const [randomIcon, setRandomIcon] = useState("hand-dots");
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+    }
+
     const handleClick = () => {
-      console.log("click");
-      console.log(fas);
+        console.log("click");
+        let icons = Object.entries(fas);
+        // console.log(Math.floor(Math.random() * icons.length));
+        let randomIndex = Math.floor(Math.random() * icons.length);
+        // console.log(icons.length);
+        console.log(icons[randomIndex][1].iconName);
+        setRandomIcon(icons[randomIndex][1].iconName);
+        // for()
     };
 
     return (
@@ -38,10 +53,7 @@ function App() {
                     <Button onClick={handleClick} variant="contained">
                         PUSH ME
                     </Button>
-                    <FontAwesomeIcon
-                        size="6x"
-                        icon={icon({ name: "user-secret" })}
-                    />
+                    <FontAwesomeIcon size="6x" icon={randomIcon} />
                 </Content>
             </Container>
         </div>
